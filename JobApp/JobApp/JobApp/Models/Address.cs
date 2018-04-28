343cd5993ee7606b1225d815a80ffece7758bada
Matúš Bezek <matusbzk@gmail.com>
2018-04-28 17:53:45 +0200

    Random koment vymazaný
    Random koment vymazaný

--- JobApp/JobApp/JobApp/Models/Address.cs
+++ JobApp/JobApp/JobApp/Models/Address.cs
@@ -7,7 +7,7 @@ namespace JobApp.Models
         // parameters
 
         //TO DO: Obmedzit dlzku na 50 znakov
-        public String Street { get; set; }          //pridavam random koment, aby som videl, ako sa bude spravat visual studio
+        public String Street { get; set; }
 
         //TO DO: Obmedzit dlzku na 10 znakov
         public String Number { get; set; }
