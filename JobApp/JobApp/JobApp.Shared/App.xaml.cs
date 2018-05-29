f5770b28fa4e7a51673834cecf334e423c305351
Milan Jankovic <MilanJ@kentico.com>
2018-05-29 20:13:05 +0200

    Create sample data for DB

--- JobApp/JobApp/JobApp.Shared/App.xaml.cs
+++ JobApp/JobApp/JobApp.Shared/App.xaml.cs
@@ -1,20 +1,71 @@
-﻿using JobApp.Shared.Views;
+﻿using System;
+using JobApp.Shared.DatabaseServices;
+using JobApp.Shared.Models;
+using JobApp.Shared.Services;
+using JobApp.Shared.Views;
+using SQLite;
 using Xamarin.Forms;
+using XamarinToolkit.Interfaces.Storage;
 
 namespace JobApp
 {
 	public partial class App : Application
 	{
-		public App ()
+	    public static Guid[] CompanyGuids =
+	    {
+	        Guid.Parse("CDADA936-9C83-4067-9A59-772EC83E1DB0"),
+	        Guid.Parse("1E521CE5-2687-41FF-8D47-45DE24A94D46"),
+	        Guid.Parse("E169494E-1B91-4053-AEA2-B4E1555B5B9C"),
+	    };
+
+        public static Guid[] ContactGuids =
+	    {
+	        Guid.Parse("CDADA936-9C83-4067-9A59-772EC83E1DBB"),
+	        Guid.Parse("1E521CE5-2687-41FF-8D47-45DE29A94D46"),
+	        Guid.Parse("E169494E-1B91-4E53-AEA2-B4E1555B5B9C"),
+	    };
+
+	    public static Contact[] Contacts =
+	    {
+	        new Contact
+	        {
+	            Id = ContactGuids[0],
+	            CompanyId = CompanyGuids[0],
+	            Email = "SamMolek@Bloh.Glog",
+	            Name = "Sam Molek",
+	            Phone = "157201652"
+	        },
+	        new Contact
+	        {
+	            Id = ContactGuids[1],
+	            CompanyId = CompanyGuids[1],
+	            Email = "WendyWolowitz@Wloh.Wlah",
+	            Name = "Wendy Wolowitz",
+	            Phone = "778778822"
+	        },
+	        new Contact
+	        {
+	            Id = ContactGuids[2],
+	            CompanyId = CompanyGuids[2],
+	            Email = "Johnny@JohnnySolver.com",
+	            Name = "JohnnySolever",
+	            Phone = "000000000"
+	        },
+	    };
+
+    public App ()
 		{
 			InitializeComponent();
 
 			base.MainPage = new NavigationPage(new JobOfferListView());
 		}
 
-		protected override void OnStart ()
+		protected override async void OnStart ()
 		{
-			// Handle when your app starts
+		    var db = new SQLiteAsyncConnection(
+		        DependencyService.Get<ISQLiteConnectionStringFactory>().Create("databName"));
+            await db.CreateTablesAsync<Contact, JobOffer, Address, Company, Interview>();
+		    // await db.InsertAllAsync(Contacts);
 		}
 
 		protected override void OnSleep ()
@@ -26,5 +77,10 @@ namespace JobApp
 		{
 			// Handle when your app resumes
 		}
+
+	    private void PopulateDb()
+	    {
+
+	    }
 	}
 }
