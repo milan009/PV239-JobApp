88f6f3e34b60bf5cb5e29132daf8acc80b65bb3d
Miroslav Kubus <kubus.miro@gmail.com>
2018-06-01 12:21:10 +0200

    Add validation of email and phone

--- JobApp/JobApp/JobApp.Shared/Validators/EmailValidatorBehavior.cs
+++ JobApp/JobApp/JobApp.Shared/Validators/EmailValidatorBehavior.cs
@@ -11,6 +11,7 @@ namespace JobApp.Shared.Validators
     {
         const string EmailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                                   @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+        public bool IsValid { get; private set; } = true;
 
         protected override void OnAttachedTo(Entry bindable)
         {
@@ -20,9 +21,8 @@ namespace JobApp.Shared.Validators
 
         void HandleTextChanged(object sender, TextChangedEventArgs e)
         {
-            bool isValid = false;
-            isValid = (Regex.IsMatch(e.NewTextValue, EmailRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)));
-            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+           IsValid = (Regex.IsMatch(e.NewTextValue, EmailRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)));
+           ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
         }
 
         protected override void OnDetachingFrom(Entry bindable)
